e45112aeca933b7cf788fe1f16b7dc9c2ccf051b
Oleksii Ustinov <ustinovoleksij@gmail.com>
2021-12-05 17:49:07 +0200

    Added index.html and index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+function askNumbers() {
+    const firstNumber = +prompt(message: 'Type first number');
+ const secondNumber = +prompt(message: 'Type second number');
+ const operator = prompt(message: 'Enter operator (either +, -, * or / ');
+ const result =('This is the result');
+ if (operator==='+') {
+     alert(add(firstNumber,secondNumber));
+ } else if (operator==='-') {
+     alert(subtraction(firstNumber,secondNumber));
+     }
+}
+ 
+  function addition(firstNumber, secondNumber){
+    return result=firstNumber+secondNumber ;
+}
+
+      
+function subtraction(firstNumber,secondNumber){
+   return result=firstNumber-secondNumber ;
+
+}
+
+function multiplication(firstNumber,secondNumber){
+    
+    return result=firstNumber*secondNumber ;
+    
+}
+function division(firstNumber, secondNumber){
+    
+     return result=firstNumber/secondNumber;
+}
+
+}
+console.log(firstNumber, secondNumber, operator);
