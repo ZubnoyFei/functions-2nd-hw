094948da754abc6b4d1c58b7818436c5e620921a
ZubnoyFei <93076661+ZubnoyFei@users.noreply.github.com>
2021-12-07 21:07:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,44 @@
 function askNumbers() {
-  const firstNumber = +prompt('Type first number');
- const secondNumber = +prompt('Type second number');
- const operator = prompt('Enter operator (either +, -, * or / '));
- const result =('This is the result');
-
- 
-  function addition(firstNumber, secondNumber){
-    return result=firstNumber+secondNumber ;
-}
-
-      
-function subtraction(firstNumber,secondNumber){
-   return result=firstNumber-secondNumber ;
-
-}
-
-function multiplication(firstNumber,secondNumber){
-    
-    return result=firstNumber*secondNumber ;
-    
-}
-function division(firstNumber, secondNumber){
-    
-     return result=firstNumber/secondNumber;
-}
-
-}
+    const firstNumber = +prompt('Type first number');
+    const secondNumber = +prompt('Type second number');
+    const operator = prompt('Enter operator (either +, -, * or / )');
+    const result = ('This is the result');
 
 
-if (operator==='+') {
-    alert(add(firstNumber,secondNumber));
-} else if (operator==='-') {
-    alert(subtraction(firstNumber,secondNumber));
+
+    if (operator === '+') {
+        alert(addition(firstNumber, secondNumber));
+    } else if (operator === '-') {
+        alert(subtraction(firstNumber, secondNumber));
     }
-    else if (operator==='*') {
-       alert(multiplication(firstNumber,secondNumber));
-       }
-       else if (operator==='/') {
-           alert(division(firstNumber,secondNumber));
-       }
+    else if (operator === '*') {
+        alert(multiplication(firstNumber, secondNumber));
+    }
+    else if (operator === '/') {
+        alert(division(firstNumber, secondNumber));
+
+    }
+    console.log(result);
 }
-console.log(firstNumber, secondNumber, operator);
+
+askNumbers();
+
+function addition(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+
+function subtraction(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+
+}
+
+function multiplication(firstNumber, secondNumber) {
+
+    return firstNumber * secondNumber;
+
+}
+function division(firstNumber, secondNumber) {
+
+    return  firstNumber / secondNumber;
+}
